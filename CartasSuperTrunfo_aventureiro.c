b4b7d658b6a2ae2b0887a14073bbc546d32c932e
rodrigocra1 <155087984+rodrigocra1@users.noreply.github.com>
2025-02-25 16:09:17 +0000

    Acrescentado o "CartasSuperTrunfo"sem loop, para satisfazer o desafio novato. Acrescentado também o "CartasSuperTrunfo_aventureiro, em resposta ao desafio nivel aventureiro.

--- CartasSuperTrunfo_aventureiro.c
+++ CartasSuperTrunfo_aventureiro.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
+#include <string.h>
 
-/*Ja havia feito o código no meu repositório pessoal,
-apenas copiei e colei aqui*/
+/* Essa versão pode ser considerada mais simples;
+ela imprime cada carta logo após o input do usuário. */
 
 void criar_carta(); // função de criar carta
 void print_carta(); // função de imprimir carta
 void criar_carta2(); // função de criar carta2
 
 // Definindo variáveis carta 1
-char estado, codigo_carta[50], cidade[100];
-int populacao, pt_turisticos;
-float area, pib;
-int carta = 1;
+char estado, codigo_carta[50], cidade[100], estado2, codigo_carta2[50], cidade2[50];
+int populacao, pt_turisticos, populacao2, pt_turisticos2;
+float area, pib, area2, pib2;
+int carta = 1, carta2 = 2;
 // Definindo variáveis carta 2
-char estado2, codigo_carta2[50], cidade2[50];
-int populacao2, pt_turisticos2;
-float area2, pib2;
-int carta2 = 2;
+
 
 int main(void)
 {
@@ -67,6 +65,8 @@ void criar_carta(void)
 
 void print_carta(void)
 {
+    float densidade = populacao / area, densidade2 = populacao2 / area2, pibpc = pib / populacao, pibpc2 = pib2 / populacao2;
+
     printf("Carta: %d\n", carta); // imprime o número da carta
 
     // imprime resultados dos inputs:
@@ -75,9 +75,11 @@ void print_carta(void)
     printf("Código da carta: %3s\n", codigo_carta);
     printf("Cidade: %s\n", cidade);
     printf("População: %d\n", populacao);
-    printf("Área: %.2f\n", area);
+    printf("Área: %.2fkm2 \n", area);
     printf("PIB: %.2f\n", pib);
     printf("Pontos turísticos: %d\n", pt_turisticos);
+    printf("Densidade Populacional: %.2f hab/km2 \n", densidade);
+    printf("PIB per capita: %.2f reais\n", pibpc);
     printf("\n");
     
     printf("Carta: %d\n", carta2); // imprime o número da carta
@@ -87,9 +89,11 @@ void print_carta(void)
     printf("Código da carta: %3s\n", codigo_carta2);
     printf("Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
-    printf("Área: %.2f\n", area2);
+    printf("Área: %.2fkm2 \n", area2);
     printf("PIB: %.2f\n", pib2);
     printf("Pontos turísticos: %d\n", pt_turisticos2);
+    printf("Densidade Populacional: %.2f hab/km2 \n", densidade2);
+    printf("PIB per capita: %.2f reais\n", pibpc2);
 }
 void criar_carta2(void)
 {
